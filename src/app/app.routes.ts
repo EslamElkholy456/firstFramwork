b4274e16a7ad2128146f88a4f8e7dbd39c8a8020
DESKTOP-4T2I8F1\LENOVO <eslamelkholy577@gmail.com>
2025-01-11 17:56:42 +0200

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import HomeComponent from '../components/home/home.component';
+import { ContactComponent } from '../components/contact/contact.component';
+import { FooterComponent } from '../components/footer/footer.component';
+import { BlogComponent } from '../components/blog/blog.component';
+import { AboutComponent } from '../components/about/about.component';
+import { NotFoundComponent } from '../components/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:'', redirectTo:'home', pathMatch:'full'},
+  {path:'home', component:HomeComponent, title:'Home'},
+  {path:'contact', component:ContactComponent, title:'Contact'},
+  {path:'footer',component:FooterComponent, title:'footer'},
+  {path:'blog',component:BlogComponent, title:'Blog'},
+  {path:'about',component:AboutComponent, title:'About'},
+  {path:'**',component:NotFoundComponent },
+];
